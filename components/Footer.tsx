2017cf0259fdd67752e500e8221866c643b695e0
elnobun <elnobun@gmail.com>
2021-02-20 09:27:23 +0000

    Added Footer Component, and code refactoring.

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <footer className="text-sm">Copyright &copy; {new Date().getFullYear()} - ellisenobun
+        </footer>
+    )
+}
+
+export default Footer
