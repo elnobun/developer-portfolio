e81e5aaa904e7c4c67cfc7d24c2e473a1aa9f291
elnobun <elnobun@gmail.com>
2021-03-14 17:46:38 +0000

    Code refactoring

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
 
     return (
         <>
-            <div className="rounded-t-lg bg-black overflow-hidden">
+            <div className="rounded-t-lg bg-black overflow-hidden flex justify-center">
                 <Image className="mx-auto"
                        src="https://www.ellisenobun.com/static/media/ellis.a5b25a4d.png"
                        alt="Picture of the author"
@@ -36,32 +36,32 @@ const Sidebar = () => {
             </div>
 
             <div className="rounded-b-lg shadow-md bg-white dark:bg-dark-card py-4 px-4">
-                <hgroup className="lg:text-left md:text-center">
+                <hgroup className="lg:text-left md:text-center text-center">
                     <h1 className="text-2xl font-bold dark:text-gray-300">Ellis Enobun</h1>
                     <h2 className="text-md text-gray-700 dark:text-gray-300">Frontend Web Developer</h2>
                 </hgroup>
 
                 <div className="pt-8 md:flex md:flex-row md:justify-center lg:flex lg:flex-col">
-                    <div className="flex items-center mb-0.5 md:mr-4">
+                    <div className="flex items-center mb-0.5 md:mr-4 justify-center lg:justify-start">
                         <GoLocation className="text-lg dark:text-gray-300"/>
                         <span className="ml-2 md:ml-1 dark:text-gray-300">London, UK</span>
                     </div>
 
 
                     <a href="mailto:elnobun@gmail.com" target="_blank"
-                       className="flex items-center mb-0.5 hover:underline md:mr-4">
+                       className="flex items-center mb-0.5 md:mr-4 justify-center lg:justify-start">
                         <MdMail className="text-lg dark:text-gray-300"/>
                         <span className="ml-2 md:ml-1 dark:text-gray-300">elnobun@gmail.com</span>
                     </a>
 
 
                     <a href="https://github.com/elnobun" target="_parent"
-                       className="flex items-center mb-0.5 hover:underline md:mr-4">
+                       className="flex items-center mb-0.5 hover:underline md:mr-4 justify-center lg:justify-start">
                         <AiFillGithub className="text-lg dark:text-gray-300"/>
                         <span className="ml-2 md:ml-1 dark:text-gray-300">github/elnobun</span>
                     </a>
                     <a href="https://linkedin.com/in/ellisenobun/" target="_parent"
-                       className="flex items-center hover:underline md:mr-4">
+                       className="flex items-center hover:underline md:mr-4 justify-center lg:justify-start">
                         <AiFillLinkedin className="text-lg dark:text-gray-300"/>
                         <span className="ml-2 md:ml-1 dark:text-gray-300">linkedin/ellisenobun</span>
                     </a>
@@ -69,20 +69,20 @@ const Sidebar = () => {
             </div>
 
 
-            <div className="my-4">
+            <div className="my-4 dark:bg-dark-card py-4 px-4">
                 <a href="" download="name"
                    className="bg-gray-300 dark:bg-white hover:bg-gray-400 dark:hover:bg-gray-200 text-gray-800 py-2 px-4 rounded flex items-center justify-center">
                     <MdFileDownload/>
                     <span className="ml-2">Download Resume</span>
                 </a>
                 {isMounted && theme === "light" && (
-                    <div className="flex items-center justify-center pt-2">
+                    <div className="flex items-center justify-center pt-2 my-2">
                         <CgToggleOff onClick={toggleTheme} className="cursor-pointer mr-2 w-10 h-10"/>
                         <IoSunnyOutline className="w-8 h-8"/>
                     </div>
                 )}
                 {isMounted && theme === "dark" && (
-                    <div className="flex items-center justify-center pt-2">
+                    <div className="flex items-center justify-center pt-2 my-2">
                         <CgToggleOn onClick={toggleTheme} className="cursor-pointer mr-2 w-10 h-10 text-white"/>
                         <IoMdMoon className="w-6 h-6 text-white"/>
                     </div>
