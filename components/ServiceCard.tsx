23c3291896801dc2d7df93044be79e51ce377acd
Ellis <elnobun@gmail.com>
2021-04-01 22:17:35 +0100

    production deployment

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -16,7 +16,7 @@ const ServiceCard: FunctionComponent<{ service: InterfaceService }> = ({service}
             <div className="flex">
                 <Icon className="w-8 h-8 text-green-700 mr-4 dark:text-green-500"/>
                 <div>
-                    <h4 className="font-semibold text-lg mb-2 dark:text-gray-200">{title}</h4>
+                    <h3 className="font-semibold text-lg mb-2 dark:text-gray-200">{title}</h3>
                     {tech.map((item, index) =>
                         <p key={index}
                            className="text-xs inline-block bg-gray-200 rounded-full px-2 py-0.5 text-gray-700 mr-2">{item}</p>
