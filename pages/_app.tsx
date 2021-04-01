af8aaa1dffd22744edf65e3737a96062c3f37cb4
Ellis <elnobun@gmail.com>
2021-04-01 21:53:05 +0100

    production deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,6 @@ import Navbar from "../components/Navbar";
 import '../styles/globals.css'
 import {ThemeProvider} from "next-themes";
 
-// import {AnimatePresence} from "framer-motion";
-
 function MyApp({Component, pageProps, router}) {
     return (
         <ThemeProvider attribute="class">
