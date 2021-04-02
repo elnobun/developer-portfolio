137bf396cb9fae20665039f05eea90da4a648470
Ellis <elnobun@gmail.com>
2021-04-02 21:46:27 +0100

    production build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,12 +7,19 @@ class MyDocument extends Document {
                 <Head>
                     <meta name="title" content="Ellis Enobun | FrontEnd Web Developer & Designer"/>
                     <meta name="description"
-                          content="FrontEnd web developer with experience in design, developing and maintaining responsive websites. Proficient in developing user stories, use cases, user interfaces, writing and testing codes, implementing new features based on user feedback.."/>
+                          content="FrontEnd web developer with experience in design, developing and maintaining responsive websites. Proficient in developing user stories, use cases, user interfaces, writing and testing codes, implementing new features based on user feedback."/>
                     <meta name="keywords" content="FrontEnd web developer, Responsive websites, web design"/>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                    <link rel="manifest" href="/site.webmanifest"/>
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+                    <meta name="msapplication-TileColor" content="#da532c"/>
+                    <meta name="theme-color" content="#ffffff"/>
                 </Head>
                 <body className="dark:bg-dark">
-                    <Main />
-                    <NextScript />
+                    <Main/>
+                    <NextScript/>
                 </body>
             </Html>
         )
