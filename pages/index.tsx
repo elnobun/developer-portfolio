316ab773831103670d7af64003ddefb30d48e651
elnobun <elnobun@gmail.com>
2021-12-10 00:50:48 +0000

    Updated bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,19 +18,11 @@ const Home = () => {
             <div
                 className="p-4 bg-white border-t rounded-b-lg shadow-md dark:bg-dark-card dark:border-dark">
                 <div className="dark:text-gray-200">
-                    <p className="mb-3"> I am a Web Developer with experience in building and mantaining accessible and performant web products
-                        and interfaces for Enterprise Content Management Systems; that are used to author over 50+ Property, 
-                        Mortgage, E-commerce, Business, Education, and Construction sites. My idea of web development is user-centered. User data drives implementation and processes; hence I approach
-                    signed-off designs with the Mobile-first design principle and Accessibility for assistive technology.</p>
+                    <p className="mb-3">I am a Web Developer with experience in building and maintaining accessible and performant web products and interfaces for Enterprise Content Management Systems used to author over 50+ Property, Mortgage, E-commerce, Business, Education, and Construction sites. My idea of web development is user-centred. User data drives implementation and processes; hence I approach signed-off designs with the Mobile-first design principle and Accessibility for assistive technology.</p>
                      
-                    <p className="mb-3">I have experience in Web Performance Optimisation (WPO) and resolving web security issues to ensure faster loading times without sacrificing functionality, 
-                    and Search Engine Optimisation (SEO) to organically increase website traffic through search results. 
-                    I regulary work with SCSS, Tailwindcss, TypeScript/Javascript(ES6), Google Analytics, Adobe XD, and 
-                    Illustrator for UX/UI. </p>
+                    <p className="mb-3">I have experience in Web Performance Optimisation (WPO) and resolving web security issues to ensure faster loading times without sacrificing functionality, and Search Engine Optimisation (SEO) to organically increase website traffic through search results. I regularly work with SCSS, Tailwindcss, TypeScript/Javascript(ES6), Google Analytics, Adobe XD, and Illustrator for UX/UI.</p>
 
-                    <p>I am always eager to grow as a developer in a team, help people through technology, and improve my skills.
-                    I am currently improving my skills in React / NextJS, and learning about data structures and algorithms.
-                    During my spare time, I enjoy quality time with family, playing games, and reading.</p>
+                    <p>I am always eager to grow as a developer in a team, help people through technology, and improve my skills. I am currently improving my skills in React / NextJS and learning about data structures and algorithms. During my spare time, I enjoy quality time with family, playing games, and reading.</p>
                 </div>
             </div>
             <div className="my-2">
