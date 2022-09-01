3feed283d0e6413d52bec1e860a453385ee3f84d
elnobun <elnobun@gmail.com>
2022-09-01 15:27:57 +0100

    sitemap configuration

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://ellisenobun.com',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
